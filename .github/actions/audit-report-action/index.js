4ca28c34159176a1151e98f2a3eb5b701ebb5678
egdmrsy <megedemirsoy@gmail.com>
2023-11-30 19:42:27 +0300

    tes

--- .github/actions/audit-report-action/index.js
+++ .github/actions/audit-report-action/index.js
@@ -65,10 +65,6 @@ function stripAnsi(string) {
     '[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)',
     '(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-ntqry=><~]))'
     ].join('|');
-	if (typeof string !== 'string') {
-		throw new TypeError(`Expected a \`string\`, got \`${typeof string}\``);
-	}
-
 	return string.replace(new RegExp(pattern, 'g'), '');
 }
 
