7b605e5d6fa16a8ccbc1be556311ce295690ec53
egdmrsy <megedemirsoy@gmail.com>
2023-11-30 21:48:25 +0300

    tes

--- .github/actions/audit-report-action/dist/index.js
+++ .github/actions/audit-report-action/dist/index.js
@@ -30891,7 +30891,18 @@ async function runAudit(projectName) {
 function stripAnsi(string) {
   const pattern = [
     '[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)',
-    '(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-ntqry=><~]))'
+    '(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-ntqry=><~]))',
+    '\\u00B9',
+    '\\u00BA',
+    '\\u00BB',
+    '\\u00BC',
+    '\\u00C8',
+    '\\u00C9',
+    '\\u00CA',
+    '\\u00CB',
+    '\\u00CC',
+    '\\u00CD',
+    '\\u00CE'
     ].join('|');
 	return string.replace(new RegExp(pattern, 'g'), '');
 }
