d0fe06814d82ce9d752054722d2884f668184a28
egdmrsy <megedemirsoy@gmail.com>
2023-12-12 17:55:53 +0100

    test

--- .github/actions/audit-report-action/issue.js
+++ .github/actions/audit-report-action/issue.js
@@ -78,7 +78,7 @@ async function createNewIssue(vulnerabilities, vulnerabilityIdProjectMapping, is
   root.appendChild(parse('<h2 id="vulnerability-header">Present Vulnerabilities</h2>'));
   const table = root.appendChild(parse('<table></table>'));
 
-  root.lastChild().appendChild(parse('<thead><tr><th>Vulnerability ID</th><th>PkgName</th><th>Title</th><th>Severity</th><th>Status</th><th>Fixed Version</th><th>Published Date</th><th>Affects</th><th>Links</th></tr></thead>'));
+  root.lastChild.appendChild(parse('<thead><tr><th>Vulnerability ID</th><th>PkgName</th><th>Title</th><th>Severity</th><th>Status</th><th>Fixed Version</th><th>Published Date</th><th>Affects</th><th>Links</th></tr></thead>'));
   const tableBody = table.appendChild(parse('<tbody id="table-body"></tbody>'));
 
   for(const vulnerability of vulnerabilities) {
