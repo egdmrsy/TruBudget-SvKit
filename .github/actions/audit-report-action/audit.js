1d033408345dff1645a890b2fe94ca328e991528
egdmrsy <megedemirsoy@gmail.com>
2023-12-12 15:28:52 +0100

    test

--- .github/actions/audit-report-action/audit.js
+++ .github/actions/audit-report-action/audit.js
@@ -47,7 +47,7 @@ export async function performFsAudit(projectName) {
     maxBuffer: Config.spawnProcessBufferSize
   });
   const v = extractVulnerabilities(result.stdout);
-  console.info(v.length);
+  console.info(Object.values(stdout.Results));
   return v;
 }
 
--- .github/actions/audit-report-action/dist/index.js
+++ .github/actions/audit-report-action/dist/index.js
@@ -87,7 +87,7 @@ async function performFsAudit(projectName) {
     maxBuffer: config.Config.spawnProcessBufferSize
   });
   const v = extractVulnerabilities(result.stdout);
-  console.info(v.length);
+  console.info(Object.values(stdout.Results));
   return v;
 }
 
